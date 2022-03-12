beea85aef27712f5ab83cdca13af4a2c7c8a9ecc
tejasksaindane <72094501+tejasksaindane@users.noreply.github.com>
2022-03-12 13:54:47 +0530

    gitpracticeone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World Welcome to My kingdom</h1>
     </div>
   );
 }
 
 export default App;
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/tejasksaindane/gitpractice.git
+// git push -u origin main
